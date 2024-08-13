c03a78670cebd75efe54c79a7cd7ca4b62bdc93f
jusbuckingham <jus.buckingham@gmail.com>
2024-08-13 15:46:02 -0700

    making some loading changes in js

--- public/jusible.js
+++ public/jusible.js
@@ -3,7 +3,6 @@
 
   function loadWidget() {
     console.log('Initializing Jusible widget');
-
     const widgetContainer = document.createElement('div');
     widgetContainer.id = 'jusible-widget-container';
     document.body.appendChild(widgetContainer);
@@ -11,7 +10,7 @@
     const rootElement = document.createElement('div');
     widgetContainer.appendChild(rootElement);
 
-    if (window.JusibleWidget && window.ReactDOM && window.React) {
+    if (window.JusibleWidget && window.React && window.ReactDOM) {
       console.log('JusibleWidget, React, and ReactDOM are available on window');
       const { createRoot } = window.ReactDOM;
       createRoot(rootElement).render(window.React.createElement(window.JusibleWidget));
@@ -20,32 +19,20 @@
     }
   }
 
-  function loadReactAndReactDOM() {
-    const reactScript = document.createElement('script');
-    reactScript.src = 'https://unpkg.com/react@17/umd/react.production.min.js';
-    reactScript.onload = () => {
-      console.log('React loaded');
-      const reactDomScript = document.createElement('script');
-      reactDomScript.src = 'https://unpkg.com/react-dom@17/umd/react-dom.production.min.js';
-      reactDomScript.onload = () => {
-        console.log('ReactDOM loaded');
-        loadWidget();
-      };
-      reactDomScript.onerror = () => {
-        console.error('Failed to load ReactDOM');
-      };
-      document.head.appendChild(reactDomScript);
-    };
-    reactScript.onerror = () => {
-      console.error('Failed to load React');
-    };
-    document.head.appendChild(reactScript);
+  function waitForDependencies(callback) {
+    const checkInterval = setInterval(() => {
+      if (window.React && window.ReactDOM && window.JusibleWidget) {
+        clearInterval(checkInterval);
+        callback();
+      } else {
+        console.log("Waiting for JusibleWidget, React, and ReactDOM to be available...");
+      }
+    }, 100); // Check every 100ms
   }
 
-  if (window.React && window.ReactDOM) {
-    console.log('React and ReactDOM are already loaded');
+  if (window.React && window.ReactDOM && window.JusibleWidget) {
     loadWidget();
   } else {
-    loadReactAndReactDOM();
+    waitForDependencies(loadWidget);
   }
 })();
